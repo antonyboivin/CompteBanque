829ef1d3d4b3737dadc780b2f85ebf3e56ad6db4
Antony <antonyboivin@outlook.fr>
2019-07-01 14:11:25 +0200

    add virer

--- CompteBanque/src/banque/Compte.java
+++ CompteBanque/src/banque/Compte.java
@@ -1,16 +1,27 @@
 package banque;
 
 public class Compte {
-    private float solde;
+    private int solde;
 
     public Compte()
     {
-        this.solde=0.0f;
+        this.solde=0;
     }
     public float getSolde() {
         return solde;
     }
-    public void setSolde(float solde) {
+    public void setSolde(int solde) {
         this.solde = solde;
+    }
+    public void deposer(int somme){
+        solde+=somme;// équivalent à solde = solde + somme
+    }
+    public int retirer(int somme){
+        return solde-=somme;
+    }
+    public int virer(int somme, Compte compteDestination)
+    {
+        compteDestination.deposer(somme);
+        return this.retirer(somme);
     }
 }
